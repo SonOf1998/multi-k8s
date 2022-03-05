6c5a2f68af47e1daa10e2d8265fc7a9dfec6c4ae
Mate Kis <ghzman029@gmail.com>
2022-03-05 13:58:47 +0100

    lol

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            rggggggggggggggggggggggggggggggggggggggggggggggggggggggg
+            XD
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">dddddd Page</Link>
