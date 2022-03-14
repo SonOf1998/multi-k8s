f25b579fa401be4725b6d8da5a6ae122c22c46ad
Mate Kis <ghzman029@gmail.com>
2022-03-14 13:37:23 +0100

    travis fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 cd kubernetes
-kubectl -apply -f .
+kubectl apply -f .
 kubectl set image deployments/client-deployment client=mkis98=multi-client:$SHA
 kubectl set image deployments/server-deployment server=mkis98=multi-server:$SHA
 kubectl set image deployments/worker-deployment worker=mkis98=multi-worker:$SHA
